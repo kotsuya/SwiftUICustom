8537dc41f165a574adc05cb8914b8c2f0f8eeefd
Yoo <kotsuya00@gmail.com>
2024-07-07 14:53:15 +0900

    Add halfModalCustom

---
+++ SwiftUICustom/HalfModalCustom.swift
@@ -0,0 +1,18 @@
+//
+//  HalfModalCustom.swift
+//  SwiftUICustom
+//
+//  Created by YOO on 2024/07/07.
+//
+
+import SwiftUI
+
+struct HalfModalCustom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HalfModalCustom()
+}
